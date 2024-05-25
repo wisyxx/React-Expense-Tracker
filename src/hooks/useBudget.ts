87a9a74816bc2ec7e660df43d3e148361191199b
Álvaro Hernández <alvarohcolino@gmail.com>
2024-05-25 11:09:52 +0200

    Add useBudget hook

---
+++ src/hooks/useBudget.ts
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { BudgetContext } from '../contexts/BudgetContext';
+
+export const useBudget = () => {
+  const context = useContext(BudgetContext);
+
+  if (!context) {
+    throw new Error('useBudget must be used within a BudgetProvider');
+  }
+
+  return {
+    context,
+  };
+};
