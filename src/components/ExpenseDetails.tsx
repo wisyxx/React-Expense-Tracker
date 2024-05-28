f930e6eef3c6c66c6ca25d4a22202755cc3d6520
Álvaro Hernández <alvarohcolino@gmail.com>
2024-05-28 17:32:19 +0200

    Add expense details to the list items

--- src/components/ExpenseDetails.tsx
+++ src/components/ExpenseDetails.tsx
@@ -1,16 +1,28 @@
+import { useMemo } from 'react';
 import { formatDate } from '../helpers';
 import { Expense } from '../types';
 import { AmountDisplay } from './AmountDisplay';
+import { categories } from '../data/categories';
 
 type ExpenseDetailsProps = {
   expense: Expense;
 };
 
 export const ExpenseDetails = ({ expense }: ExpenseDetailsProps) => {
+  const categoryInfo = useMemo(
+    () => categories.filter((cat) => cat.id === expense.category)[0],
+    [expense]
+  );
+
   return (
-    <div className=" bg-white shadow-lg p-10 w-full border-b border-gray-200">
-      <div></div>
+    <div className=" bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-6">
       <div>
+        <img className='w-20' src={`/icon_${categoryInfo.icon}.svg`} alt="Category icon" />
+      </div>
+      <div className=' flex-1 space-y-2'>
+        <p className="text-sm font-bold uppercase text-slate-500">
+          {`Category: ${categoryInfo.name}`}
+        </p>
         <p>{expense.expenseName}</p>
         <p className=" text-slate-600 text-sm">
           {formatDate(expense.date!.toString())}
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,11 +1,11 @@
 import { Category } from '../types';
 
 export const categories: Category[] = [
-  { id: '1', name: 'Savings', icon: 'ahorro' },
-  { id: '2', name: 'Food', icon: 'comida' },
-  { id: '3', name: 'House', icon: 'casa' },
-  { id: '4', name: 'Miscellaneous', icon: 'gastos' },
-  { id: '5', name: 'Entertainment', icon: 'ocio' },
-  { id: '6', name: 'Health', icon: 'salud' },
-  { id: '7', name: 'Memberships', icon: 'suscripciones' },
+  { id: '1', name: 'Savings', icon: 'savings' },
+  { id: '2', name: 'Food', icon: 'food' },
+  { id: '3', name: 'House', icon: 'house' },
+  { id: '4', name: 'Miscellaneous', icon: 'miscellaneous' },
+  { id: '5', name: 'Entertainment', icon: 'entertainment' },
+  { id: '6', name: 'Health', icon: 'health' },
+  { id: '7', name: 'Memberships', icon: 'memberships' },
 ];
